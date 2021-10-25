127720041e68b2cfe4f8c8271343532d82524067
JErickGM <jerickgm89@gmail.com>
2021-10-24 21:41:43 -0500

    agregando la api index.php a empleados y mostrar datos en la tabla

--- js/main.js
+++ js/main.js
@@ -1,18 +1,48 @@
-var aplicacion = new function () { 
+var aplicacion = new (function () {
+    var url = "http://localhost/empleados/";
     this.nombre = document.getElementById("nombre");
     this.correo = document.getElementById("correo");
     this.empleados = document.getElementById("empleados");
 
-    this.Leer = function(){
-        var datos ="";
-        datos="<tr><td>1</td><td>JErick</td><td>jerickgm89@gmail.com</td><td>Editar | Borrar</td></tr>";
-        return this.empleados.innerHTML=datos;
-        
-    }
-    this.Agregar=function(){
+    this.Leer = function () {
+        var datos = "";
+        fetch(url)
+            .then((r) => r.json())
+            .then((respuesta) => {
+                console.log(respuesta);
+                respuesta.map(function (empleado, index, array) {
+                    datos += "<tr>";
+                    datos += "<td>" + empleado.id + "</td>";
+                    datos += "<td>" + empleado.nombre + "</td>";
+                    datos += "<td>" + empleado.correo + "</td>";
+                    datos += "<td>" + empleado.id + "</td>";
+
+                    datos += "<tr>";
+                });
+
+                return (this.empleados.innerHTML = datos);
+            })
+            .catch(console.log);
+        //datos ="<tr><td>1</td><td>JErick</td><td>jerickgm89@gmail.com</td><td>Editar | Borrar</td></tr>";
+    };
+    this.Agregar = function () {
         console.log(nombre.value);
         console.log(correo.value);
-    }
- 
-}
-aplicacion.Leer();  
+        var datosEnviar = {
+            nombre: this.nombre.value,
+            correo: this.correo.value,
+        };
+
+        fetch(url + "?insertar=1", {
+            method: "POST",
+            body: JSON.stringify(datosEnviar),
+        })
+            .then((respuesta) => respuesta.json())
+            .then((datosRespuesta) => {
+                console.log("Insertados");
+                this.Leer();
+            })
+            .catch(console.log);
+    };
+})();
+aplicacion.Leer();
